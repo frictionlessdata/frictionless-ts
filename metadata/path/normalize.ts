3e30b62da28c88f126d483d807421ff8f1389fed
roll <eskarev@gmail.com>
2025-12-18 10:33:52 +0000

    chore: Removed unused code

--- metadata/path/normalize.ts
+++ metadata/path/normalize.ts
@@ -40,38 +40,3 @@ export function normalizePath(path: string, options: { basepath?: string }) {
 
   return node.path.relative(process.cwd(), node.path.resolve(normalizedPath))
 }
-
-export function denormalizePath(path: string, options: { basepath?: string }) {
-  const isPathRemote = isRemotePath(path)
-  const isBasepathRemote = isRemotePath(options.basepath ?? "")
-
-  if (isPathRemote) {
-    return new URL(path).toString()
-  }
-
-  if (isBasepathRemote) {
-    const basepath = new URL(options.basepath ?? "").toString()
-
-    if (!path.startsWith(basepath)) {
-      throw new Error(`Path ${path} is not a subpath of ${options.basepath}`)
-    }
-
-    const relative = path.replace(`${basepath}/`, "")
-    return relative
-  }
-
-  if (!node) {
-    throw new Error("File system is not supported in this environment")
-  }
-
-  const normalizedPath = node.path.resolve(path)
-  const normalizedBasepath = node.path.resolve(options.basepath ?? "")
-
-  if (!normalizedPath.startsWith(normalizedBasepath)) {
-    throw new Error(`Path ${path} is not a subpath of ${options.basepath}`)
-  }
-
-  // The Data Package standard requires "/" as the path separator
-  const relative = node.path.relative(normalizedBasepath, normalizedPath)
-  return relative.split(node.path.sep).join("/")
-}
